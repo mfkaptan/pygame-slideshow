54a469f2fba2f0beb0ced9d10b80d29250018902
mfkaptan <mfkaptan@outlook.com>
2013-03-27 23:45:37 +0200

    eclipse plugin?

--- slideshow.py
+++ slideshow.py
@@ -1,62 +1,42 @@
 # Mustafa Kaptan
 # Slideshow programi
 
-
-# TODO : duzenle
-# TODO : images klasorunun icine konan dosyalari isimlerini kullanarak kaydir
-    # Or: 2.jpg -> 2 saniye
-
-import pygame
-try:
-    import android
-except ImportError:
-    android = None
-
-# Screen
-WIDTH=500
-HEIGHT=300
+import pygame,sys
 
 # Colors
-BLACK = (0 ,0 ,0)
-
+BLACK = (0 , 0 , 0)
 
 class Image(pygame.sprite.Sprite):
-    def __init__(self,picture,time):
+    def __init__(self, picture, time, screen):
         pygame.sprite.Sprite.__init__(self)
-        self.image=pygame.image.load("./slideshow/"+picture).convert()
-        self.time=int(time)
-        self.rect=self.image.get_rect()
-    
+        self.image = pygame.image.load("./slideshow/" + picture).convert()
+        self.time = int(time)
+        self.rect = self.image.get_rect()
+        self.rect.centerx = screen.get_width() / 2
+        self.rect.centery = screen.get_height() / 2
+            
     def update(self):
-        self.time=self.time
+        self.time = self.time
     
 def main():
     # Initialize
     pygame.init()
     
-    # Map the back button to the escape key.
-    if android:
-        android.init()
-        android.map_key(android.KEYCODE_BACK, pygame.K_ESCAPE)
-    
     # The FPS the game runs at.
     DELAY = 0
     
     # Screen
-    size=[WIDTH,HEIGHT]
-    screen=pygame.display.set_mode(size)
+    screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
     pygame.display.set_caption("Slideshow")
     
-    # List of lines
-    line_list=list
     # List of sprite images
-    slideShow=pygame.sprite.OrderedUpdates()
-    currentSlide=pygame.sprite.OrderedUpdates()
+    slideShow = pygame.sprite.OrderedUpdates()
+    currentSlide = pygame.sprite.OrderedUpdates()
     
     # Read from file and save
-    file = open("./slideshow/slideshow.sld")
-    line_list = file.readlines()
-    file.close()
+    slideFile = open("./slideshow/slideshow.sld")
+    line_list = slideFile.readlines()
+    slideFile.close()
     
     # Length of line_list
     length = len(line_list)
@@ -65,26 +45,21 @@ def main():
     # print( "There were ",length," lines in the file.")
 
     # Save lines to Image class and Slideshow list
-    for i in range (0,length,2):
-        slideImage=Image(line_list.pop(0).replace('\n',''),line_list.pop(0))
+    for i in range (0, length, 2):
+        slideImage = Image(line_list.pop(0).replace('\n', ''), line_list.pop(0), screen)
         slideShow.add(slideImage)
         
-    i=-1
+    i = -1
     
     # Use a timer to control FPS.
     clock = pygame.time.Clock()
     # Main loop    
     while True:
-        # Android-specific:
-        if android:
-            if android.check_pause():
-                android.wait_for_resume()
-            
         # Empty the list
         currentSlide.empty()
-        i+=1
+        i += 1
         
-        if i<length/2:
+        if i < length / 2:
             for image in slideShow.sprites():
                 currentSlide.add(image)
                 slideShow.remove_internal(image)
@@ -94,18 +69,18 @@ def main():
             return False
         
         for event in pygame.event.get():
-            if event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE: # If user hit ESC
+            if event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:  # If user hit ESC
                 return False
-            if event.type == pygame.QUIT: # If user clicked close 
+            if event.type == pygame.QUIT:  # If user clicked close 
                 return False
         
         # Draw the screen based on the timer.
         screen.fill(BLACK)
         for slide in currentSlide.sprites():
-            DELAY=slide.time
+            DELAY = slide.time
         currentSlide.draw(screen)
         pygame.display.flip()
-        pygame.time.delay(DELAY*1000)
+        pygame.time.delay(DELAY * 1000)
         clock.tick(25)
         
     pygame.quit()
